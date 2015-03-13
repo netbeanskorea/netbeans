02b8c9c42682338f44319f6c1fcbdb4f303062e9
netbeanskorea <netbeans.korea@gmail.com>
2015-03-13 06:24:02 -0300

    3

---
+++ totoro/private cloud.mm
@@ -0,0 +1,62 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1426233002454" ID="ID_580611363" MODIFIED="1426233010909" TEXT="private cloud">
+<node CREATED="1426234002303" ID="ID_854762615" MODIFIED="1426234004580" POSITION="left" TEXT="EMC">
+<node CREATED="1426233103349" ID="ID_1396454688" MODIFIED="1426233107588" TEXT="&#xc815;&#xc758;(EMC)">
+<node CREATED="1426233108335" ID="ID_319908278" MODIFIED="1426233128949" TEXT="cloud computing &#xd658;&#xacbd;&#xc758; &#xad6c;&#xcd95; &#xbaa8;&#xb378; &#xc911; &#xd558;&#xb098;"/>
+<node CREATED="1426233134063" ID="ID_267884761" MODIFIED="1426233187457" TEXT="&#xc694;&#xccad;&#xc5d0; &#xb530;&#xb77c; IT&#xbd80;&#xc11c;&#xc5d0;&#xc11c; application, computing, storage, networking &#xb4f1;&#xc758;&#xa;IT &#xb9ac;&#xc18c;&#xc2a4;&#xb97c; &#xc81c;&#xacf5;&#xd558;&#xace0; &#xc0ac;&#xc6a9;&#xb7c9;&#xc744; &#xae30;&#xc900;&#xc73c;&#xb85c; &#xbe44;&#xc6a9;&#xc744; &#xccad;&#xad6c;&#xd558;&#xb294; &#xbc29;&#xc2dd;"/>
+</node>
+<node CREATED="1426233254526" ID="ID_924300355" MODIFIED="1426233259722" TEXT="how charge">
+<node CREATED="1426233260744" ID="ID_597571258" MODIFIED="1426233273346" TEXT="&#xc2e4;&#xc81c; &#xc0ac;&#xc6a9;&#xb7c9;&#xc5d0; &#xb530;&#xb77c; &#xbe44;&#xc6a9; &#xccad;&#xad6c;"/>
+</node>
+<node CREATED="1426233192411" ID="ID_217399660" MODIFIED="1426233197038" TEXT="who">
+<node CREATED="1426233197842" ID="ID_1976916361" MODIFIED="1426233236358" TEXT="&#xc911;&#xac04; &#xaddc;&#xbaa8;&#xc758; &#xae30;&#xc5c5;&#xacfc; &#xb300;&#xae30;&#xc5c5; &#xbc0f; &#xc815;&#xbd80;&#xae30;&#xad00;&#xc740; &#xa;&#xbe44;&#xc988;&#xb2c8;&#xc2a4; &#xb300;&#xc751; &#xb2a5;&#xb825;&#xacfc; &#xd6a8;&#xc728;&#xc131;&#xc744; &#xb192;&#xc774;&#xace0; &#xacbd;&#xc7c1;&#xb825;&#xc744; &#xd655;&#xbcf4;&#xd558;&#xae30; &#xc704;&#xd574; &#xa;Private Cloud &#xd658;&#xacbd;&#xc744; &#xad6c;&#xcd95;"/>
+</node>
+<node CREATED="1426233495622" ID="ID_1006718335" MODIFIED="1426233497428" TEXT="&#xc81c;&#xacf5; &#xc11c;&#xbe44;&#xc2a4;">
+<node CREATED="1426233498230" ID="ID_110169500" MODIFIED="1426233500203" TEXT="SaaS"/>
+<node CREATED="1426233501206" ID="ID_395963023" MODIFIED="1426233506250" TEXT="IaaS"/>
+<node CREATED="1426233507110" ID="ID_951224080" MODIFIED="1426233511026" TEXT="PaaS"/>
+</node>
+<node CREATED="1426233279796" ID="ID_249623904" MODIFIED="1426233283649" TEXT="&#xd6a8;&#xacfc;">
+<node CREATED="1426233285252" ID="ID_1435767923" MODIFIED="1426233351479" TEXT="&#xc870;&#xc9c1;&#xc785;&#xc7a5;&#xc5d0;&#xc11c;&#xb294; &#xd504;&#xb85c;&#xc81d;&#xd2b8;&#xb97c; &#xbcf4;&#xb2e4; &#xc2e0;&#xc18d;&#xd558;&#xac8c; &#xcc29;&#xc218;&#xd558;&#xace0; &#xc0c8;&#xb85c;&#xc6b4; &#xae30;&#xb2a5;&#xacfc; &#xb9e4;&#xcd9c; &#xae30;&#xd68c;&#xb97c; &#xbe60;&#xb974;&#xac8c; &#xd65c;&#xc6a9;&#xd560; &#xc218; &#xc788;&#xc73c;&#xba70; &#xa;&#xc2dc;&#xc7a5;&#xc758; &#xbcc0;&#xd654;&#xc5d0; &#xbbfc;&#xcca9;&#xd558;&#xac8c; &#xb300;&#xc751;&#xd560; &#xc218; &#xc788;&#xc73c;&#xbbc0;&#xb85c; Private Cloud &#xd658;&#xacbd;&#xc73c;&#xb85c; &#xc804;&#xd658;&#xd558;&#xba74; IT&#xc870;&#xc9c1;&#xc774; &#xa;&#xb9ce;&#xc740; &#xbe44;&#xc6a9;&#xc744; &#xc218;&#xbc18;&#xd558;&#xb294; &#xc870;&#xc9c1;&#xc5d0;&#xc11c; &#xc804;&#xb7b5;&#xc801;&#xc778; &#xd30c;&#xd2b8;&#xb108;&#xb85c; &#xd0c8;&#xbc14;&#xafc8;"/>
+</node>
+<node CREATED="1426233355679" ID="ID_24578377" MODIFIED="1426233359935" TEXT="&#xc791;&#xb3d9; &#xbc29;&#xc2dd;">
+<node CREATED="1426233361615" ID="ID_519881893" MODIFIED="1426233380892" TEXT="&#xac00;&#xc0c1;&#xd654; &#xd658;&#xacbd;&#xc5d0; &#xcd5c;&#xc801;&#xd654;&#xb418;&#xace0; &#xc644;&#xbcbd;&#xd558;&#xac8c; &#xd1b5;&#xd569;&#xb41c; &#xc778;&#xd504;&#xb77c; &#xc2a4;&#xd2b8;&#xb7ed;&#xcc98;&#xac00; &#xd544;&#xc694;"/>
+<node CREATED="1426233393406" ID="ID_735168690" MODIFIED="1426233418993" TEXT="&#xb2e8;&#xc77c; &#xc870;&#xc9c1;&#xc774; IT&#xb9ac;&#xc18c;&#xc2a4; &#xc18c;&#xc720;, &#xb0b4;&#xbd80;&#xc801;&#xc73c;&#xb85c; &#xacf5;&#xc720;&#xd558;&#xba74;&#xc11c; &#xc804;&#xc6a9;&#xc73c;&#xb85c; &#xc0ac;&#xc6a9;&#xd558;&#xba70; &#xb9ac;&#xc18c;&#xc2a4;&#xac00; &#xc778;&#xd130;&#xb137; &#xb610;&#xb294; LAN&#xc744; &#xd1b5;&#xd574; &#xc11c;&#xbe44;&#xc2a4;&#xb85c; &#xc81c;&#xacf5;"/>
+<node CREATED="1426233420227" ID="ID_1841938241" MODIFIED="1426233438551" TEXT="&#xc77c;&#xbc18;&#xc801;&#xc73c;&#xb85c; &#xb9ac;&#xc18c;&#xc2a4;&#xb294; &#xc870;&#xc9c1; &#xb0b4;&#xc5d0; &#xad6c;&#xcd95;&#xb418;&#xace0; &#xad00;&#xb9ac;&#xb418;&#xc9c0;&#xb9cc; &#xd0c0;&#xc0ac;&#xc5d0;&#xc11c; &#xd638;&#xc2a4;&#xd2b8;&#xd558;&#xac70;&#xb098; &#xad00;&#xb9ac;&#xd560; &#xc218;&#xb3c4; &#xc788;&#xc2b5;&#xb2c8;&#xb2e4;."/>
+<node CREATED="1426233439898" ID="ID_836673618" MODIFIED="1426233458750" TEXT="EMC&#xc5d0;&#xc11c;&#xb294; &#xc774;&#xb7ec;&#xd55c; &#xbc29;&#xc2dd;&#xc744; Virtual Private Cloud &#xb77c;&#xace0; &#xce6d;&#xd568;"/>
+<node CREATED="1426233463633" ID="ID_1875811338" MODIFIED="1426233488548" TEXT="EMC&#xc758; Velocity &#xd504;&#xb85c;&#xadf8;&#xb7a8;&#xc5d0; &#xcc38;&#xc5ec;&#xd558;&#xb294; &#xb300;&#xbd80;&#xbd84;&#xc758; &#xc11c;&#xbe44;&#xc2a4; &#xacf5;&#xae09;&#xc5c5;&#xccb4;&#xb294; &#xc911;&#xac04; &#xaddc;&#xbaa8; &#xae30;&#xc5c5;&#xacfc; &#xb300;&#xae30;&#xc5c5;&#xc5d0; &#xac00;&#xc0c1; &#xd504;&#xb77c;&#xc774;&#xbe57; &#xd074;&#xb77c;&#xc6b0;&#xb4dc;&#xb97c; &#xc81c;&#xacf5;&#xd568;"/>
+</node>
+<node CREATED="1426233520486" ID="ID_519979472" MODIFIED="1426233524538" TEXT="&#xc787;&#xc810;">
+<node CREATED="1426233525573" ID="ID_1367419832" MODIFIED="1426233542593" TEXT="&#xbe60;&#xb978; IT &#xd658;&#xacbd; &#xad6c;&#xcd95;&#xacfc; &#xc9e7;&#xc740; &#xd22c;&#xc790; &#xd68c;&#xc218; &#xae30;&#xac04;"/>
+<node CREATED="1426233543723" ID="ID_712087308" MODIFIED="1426233931601" TEXT="&#xc7a5;&#xc18c;&#xc5d0; &#xad6c;&#xc560;&#xbc1b;&#xc9c0; &#xc54a;&#xc740; &#xc5b4;&#xd50c;&#xb9ac;&#xcf00;&#xc774;&#xc158;,Desktop, &#xbc0f; &#xc815;&#xbcf4; &#xc561;&#xc138;&#xc2a4;"/>
+<node CREATED="1426233932500" ID="ID_648974052" MODIFIED="1426233937459" TEXT="&#xb192;&#xc740; IT &#xd22c;&#xc790; &#xd65c;&#xc6a9;&#xb3c4;"/>
+<node CREATED="1426233938373" ID="ID_1274985218" MODIFIED="1426233946339" TEXT="&#xbe60;&#xb978; &#xd655;&#xc7a5; &#xb610;&#xb294; &#xc218;&#xc694;&#xbcc0;&#xd654;&#xc5d0; &#xb530;&#xb978; &#xc7ac;&#xd560;&#xb2f9;"/>
+<node CREATED="1426233947165" ID="ID_970794485" MODIFIED="1426233952858" TEXT="&#xbcf4;&#xc548; &#xbc0f; &#xc815;&#xbcf4;&#xc790;&#xc0b0; &#xbcf4;&#xd638;&#xac15;&#xd654;"/>
+<node CREATED="1426233953694" ID="ID_941975853" MODIFIED="1426233961996" TEXT="IT &#xc778;&#xb825; &#xbc0f; &#xae30;&#xc5c5; &#xc804;&#xbc18;&#xc758; &#xc0dd;&#xc0b0;&#xc131; &#xc99d;&#xc9c4;"/>
+<node CREATED="1426233963111" ID="ID_1474145407" MODIFIED="1426233971124" TEXT="&#xc5d0;&#xb108;&#xc9c0;, &#xc778;&#xd504;&#xb77c;&#xc2a4;&#xd2b8;&#xb7ed;&#xcc98; &#xbc0f; &#xc124;&#xbe44; &#xbe44;&#xc6a9; &#xc808;&#xac10;"/>
+</node>
+</node>
+<node CREATED="1426234023337" ID="ID_1735674318" MODIFIED="1426234034157" POSITION="right" TEXT="Hostway">
+<node CREATED="1426234035122" ID="ID_414444073" MODIFIED="1426234038566" TEXT="&#xc815;&#xc758;">
+<node CREATED="1426234039424" ID="ID_1460291823" MODIFIED="1426234058231" TEXT="Private Cloud&#xb780; &#xac1c;&#xbcc4; &#xae30;&#xc5c5;&#xc774; &#xb0b4;&#xbd80; &#xd658;&#xacbd;&#xc5d0; &#xb9de;&#xac8c; &#xb2e8;&#xb3c5;&#xc801;&#xc73c;&#xb85c; &#xad6c;&#xcd95;&#xd558;&#xb294; &#xc778;&#xd504;&#xb77c; &#xd074;&#xb77c;&#xc6b0;&#xb4dc; &#xc11c;&#xbe44;&#xc2a4;&#xc785;&#xb2c8;&#xb2e4;."/>
+</node>
+<node CREATED="1426234081954" ID="ID_261098564" MODIFIED="1426234084802" TEXT="&#xd2b9;&#xc9d5;">
+<node CREATED="1426234085362" ID="ID_1401793561" MODIFIED="1426234091367" TEXT="&#xae30;&#xc5c5;&#xc758; &#xc804;&#xccb4; &#xc2dc;&#xc2a4;&#xd15c;&#xc744; &#xd1b5;&#xd569;&#xad00;&#xb9ac;"/>
+<node CREATED="1426234092252" ID="ID_271263408" MODIFIED="1426234097906" TEXT="&#xc2dc;&#xc2a4;&#xd15c; &#xd655;&#xc7a5;&#xc131;, &#xbbfc;&#xcca9;&#xc131; &#xd658;&#xacbd; &#xad6c;&#xcd95;"/>
+<node CREATED="1426234098725" ID="ID_485084609" MODIFIED="1426234105524" TEXT="&#xc548;&#xc804;&#xc131; &#xbc0f; &#xbcf4;&#xc548;&#xc131; &#xac15;&#xd654;"/>
+<node CREATED="1426234106315" ID="ID_169999662" MODIFIED="1426234112152" TEXT="&#xb808;&#xac70;&#xc2dc; &#xc778;&#xd504;&#xb77c;&#xc640; &#xc5f0;&#xacc4; &#xac00;&#xb2a5;"/>
+<node CREATED="1426234112939" ID="ID_655102186" MODIFIED="1426234120112" TEXT="On Premise Private Cloud">
+<node CREATED="1426234250585" ID="ID_982947901" MODIFIED="1426234326427" TEXT="On Premise: (in-house)&#xa;&#xc778;&#xd130;&#xb137; &#xb124;&#xd2b8;&#xc6cc;&#xd06c;&#xc5d0; &#xc5f0;&#xacb0;&#xb41c; &#xc11c;&#xbc84;&#xd31c;&#xc774;&#xb098; &#xd074;&#xb77c;&#xc6b0;&#xb4dc; &#xb4f1;&#xc758; &#xa;&#xc6d0;&#xaca9; &#xd658;&#xacbd;&#xc5d0;&#xc11c; &#xc0ac;&#xc6a9;&#xd558;&#xb294; &#xac83;&#xc774; &#xc544;&#xb2c8;&#xb77c; &#xa;&#xac74;&#xbb3c;&#xc5d0;&#xc11c; &#xc77c;&#xd558;&#xb294; &#xc9c1;&#xc6d0; &#xb610;&#xb294; &#xb2e8;&#xccb4;&#xc5d0;&#xc11c; &#xc124;&#xce58;, &#xc2e4;&#xd589;&#xd558;&#xb294; SW"/>
+<node CREATED="1426234396990" ID="ID_1056424788" MODIFIED="1426234471843" TEXT="cloud&#xc640; on-premise&#xc758; &#xcef4;&#xd4e8;&#xd305;, &#xc2a4;&#xd1a0;&#xb9ac;&#xc9c0;. &#xd2b8;&#xb79c;&#xc7ad;&#xc158; &#xbe44;&#xc6a9;"/>
+<node CREATED="1426234474200" ID="ID_753098184" MODIFIED="1426234502190" TEXT="on-premise&#xc758; &#xc228;&#xc740; &#xbe44;&#xc6a9;: &#xb0b4;&#xbd80; &#xc778;&#xb825; &#xbe44;&#xc6a9;">
+<node CREATED="1426234502892" ID="ID_1222378833" MODIFIED="1426234537668" TEXT="&#xc11c;&#xbe44;&#xc2a4;&#xb97c; &#xc720;&#xc9c0;&#xd558;&#xb294; SM&#xc774;&#xb098; IT &#xb2f4;&#xb2f9;&#xc790;&#xc758; &#xbe44;&#xc6a9; &#xd3ec;&#xd568;"/>
+<node CREATED="1426234547347" ID="ID_42737020" MODIFIED="1426234586250" TEXT="&#xc778;&#xb825;&#xc720;&#xc9c0;&#xbe44;&#xc6a9;&#xc73c;&#xb85c; &#xb124;&#xd2b8;&#xc6cc;&#xd06c; &#xbe44;&#xc6a9;, &#xc2ec;&#xc9c0;&#xc5b4; &#xac1c;&#xc778;&#xc0ac;&#xc6a9; &#xc0ac;&#xbb34;&#xc2e4;&#xacf5;&#xac04;&#xbe44;&#xc6a9; &#xd3ec;&#xd568;"/>
+<node CREATED="1426234587900" ID="ID_1617242653" MODIFIED="1426234587900" TEXT=""/>
+</node>
+</node>
+<node CREATED="1426234121155" ID="ID_284414341" MODIFIED="1426234128673" TEXT="In IDC Private Cloud"/>
+</node>
+</node>
+</node>
+</map>
